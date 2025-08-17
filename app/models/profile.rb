c470909daf7909656c0d2004f6124a6f02ed20db
Bruno Sacco <brusacco@gmail.com>
2025-08-17 08:54:30 -0300

    Refactor Profile model validations and update ActiveAdmin initializer for consistency

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,7 @@
 class Profile < ApplicationRecord
   validates :name, :slug, presence: true
   validates :slug, uniqueness: true
+  enum :profile_type, { personal: 0, business: 1 }
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at id name slug updated_at profile_type]
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ActiveAdmin.setup do |config|
   # == Site Title
   #
@@ -6,7 +8,7 @@ ActiveAdmin.setup do |config|
   # by extracting the _site_header partial into your project
   # to use your own logo, styles, etc.
   #
-  config.site_title = "Socialhub"
+  config.site_title = 'SocialHub'
 
   # == Load Paths
   #
@@ -127,7 +129,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -160,7 +162,7 @@ ActiveAdmin.setup do |config|
   # You can exclude possibly sensitive model attributes from being displayed,
   # added to forms, or exported by default by ActiveAdmin
   #
-  config.filter_attributes = [ :encrypted_password, :password, :password_confirmation ]
+  config.filter_attributes = %i[encrypted_password password password_confirmation]
 
   # == Localize Date/Time Format
   #
